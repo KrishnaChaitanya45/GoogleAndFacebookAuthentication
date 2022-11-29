5a5381a33d39b9091b965c947ae5c7d9ae32c844
Krishna Chaitanya Kommera <kommerakrishnachaitanya@gmail.com>
2022-11-29 22:29:33 +0530

    backend changed - 7

--- backend/passport.js
+++ backend/passport.js
@@ -7,7 +7,7 @@ passport.use(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "/auth/google/callback",
+			callbackURL: "https://auth-server-pwva.onrender.com/auth/google/callback",
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
@@ -22,7 +22,7 @@ passport.use(
 		{
 			clientID: process.env.FACEBOOK_ID,
 			clientSecret: process.env.FACEBOOK_SECRET,
-			callbackURL: "/auth/facebook/callback",
+			callbackURL: "https://auth-server-pwva.onrender.com/auth/facebook/callback",
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
