217b8b7725038622150e53cfbcd1e5f3545b2c8a
Krishna Chaitanya Kommera <kommerakrishnachaitanya@gmail.com>
2022-11-30 19:25:59 +0530

    backend-changed 8

--- backend/passport.js
+++ backend/passport.js
@@ -7,7 +7,7 @@ passport.use(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "/auth/google/callback",
+			callbackURL: "https://fliqa-auth.netlify.app/auth/google/callback",
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
@@ -22,7 +22,7 @@ passport.use(
 		{
 			clientID: process.env.FACEBOOK_ID,
 			clientSecret: process.env.FACEBOOK_SECRET,
-			callbackURL: "/auth/facebook/callback",
+			callbackURL: "https://fliqa-auth.netlify.app/auth/facebook/callback",
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
